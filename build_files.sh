7b65b6cc6eb2db8b1980f28ad6200afefe024491
Arsen <ne.molchoun@gmail.com>
2023-07-21 20:01:56 +0400

    vercel build config

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.9 pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 echo "Make migrations..."
 python3.9 prediction_project/manage.py makemigrations
--- prediction_project/prediction_project/settings.py
+++ prediction_project/prediction_project/settings.py
@@ -147,9 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mlmodel/static')
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
