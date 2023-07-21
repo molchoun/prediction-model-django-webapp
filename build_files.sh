6c9e9f14aa8c27fbc80ade30675bd1a46bae8539
Arsen <ne.molchoun@gmail.com>
2023-07-21 20:08:15 +0400

    vercel build config

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 echo "Make migrations..."
 python3.9 prediction_project/manage.py makemigrations
-python3.9 prediction_project/manage.py migrate && 1 && 0
+python3.9 prediction_project/manage.py migrate
 echo "Collect static"
 python3.9 prediction_project/manage.py collectstatic --no-input --clear
 echo "BUILD END"
