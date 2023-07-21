e00ebaa16282a85c4e03c0786ee15121895564cb
Arsen <ne.molchoun@gmail.com>
2023-07-21 19:54:25 +0400

    vercel build config

--- prediction_project/prediction_project/settings.py
+++ prediction_project/prediction_project/settings.py
@@ -148,7 +148,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'mlmodel/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
