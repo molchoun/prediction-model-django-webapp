e24bc9c29c9ec16478d8a2c52038c8365f38c387
Arsen <ne.molchoun@gmail.com>
2023-07-21 18:15:36 +0400

    vercel build config

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --no-input --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --no-input --clear
 echo "BUILD END"
